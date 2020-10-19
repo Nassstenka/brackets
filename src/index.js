c651fe46e0857bb6d4aa4d4d7db8aae4e7feee02
Anastasiya Satsiuk <nastyasatsyuk@yandex.by>
2020-10-20 00:40:45 +0300

    fix: make function work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let charArray = str.split('');
+  let openBrackets = [];
+  let closeBrackets = [];
+  let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < charArray.length; i++) {
+      if (openBrackets.includes(charArray[i]) && !closeBrackets.includes(charArray[i])) {
+        stack.push(charArray[i]);
+        continue;
+      }
+      else if (!openBrackets.includes(charArray[i]) && closeBrackets.includes(charArray[i])) {
+          if (stack.length !== 0) {
+            let top = stack.pop();
+            if (openBrackets.indexOf(top) !== closeBrackets.indexOf(charArray[i])) return false;
+          }
+          else return false;
+      }
+      else if (openBrackets.includes(charArray[i]) && closeBrackets.includes(charArray[i])) {
+        if (stack.length !== 0) {
+            let top = stack.pop();
+            if (openBrackets.indexOf(top) !== closeBrackets.indexOf(charArray[i])) {
+                stack.push(top);
+                stack.push(charArray[i]);
+                continue;
+            }
+        }
+        else {
+            stack.push(charArray[i]);
+            continue;
+        }
+      }
+  }
+
+  if (stack.length !== 0) return false;
+
+  return true;
 }
